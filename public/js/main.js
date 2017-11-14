92c11036c664e5b1308ed06837b1ecabbf54c4ce
gamuo <gamu.ha.machiwo@gmail.com>
2017-11-14 23:18:43 +0900

    tab -> space

--- public/js/main.js
+++ public/js/main.js
@@ -7,10 +7,10 @@ var row_number = 0;
 * supple:
 **********************/
 function insert_log(insert_element)
-	{
-		var log = document.getElementById('log');
-		log.append(insert_element);
-	}
+    {
+        var log = document.getElementById('log');
+        log.append(insert_element);
+    }
 
 /***********************
 * func: jsonを取得する
@@ -19,18 +19,18 @@ function insert_log(insert_element)
 * supple:
 ***********************/
 function load_json()
-	{
+    {
 
-		var message_text = document.getElementById('message_text');
-		message_text.innerText = 'server request...'
-		setTimeout(function() {message_text.innerText = 'waiting for data...'}, 10000)
+        var message_text = document.getElementById('message_text');
+        message_text.innerText = 'server request...'
+        setTimeout(function() {message_text.innerText = 'waiting for data...'}, 10000)
 
-		var script     = document.createElement('script');
-		script.charset = 'utf-8';
-		script.src     = 'http://www.geocities.jp/nice_popcorn/log.js';
+        var script     = document.createElement('script');
+        script.charset = 'utf-8';
+        script.src     = 'http://www.geocities.jp/nice_popcorn/log.js';
 
-		document.body.appendChild(script);
-	}
+        document.body.appendChild(script);
+    }
 
 
 /*********************
@@ -40,17 +40,17 @@ function load_json()
 * supple: コールバックで１０秒後づつに処理するため
 *********************/
 function after_seconds(n) 
-	{
-		var plus_seconds = (n+1) * 10
+    {
+        var plus_seconds = (n+1) * 10
 
-		var date = new Date();
-		date.setSeconds(date.getSeconds() + plus_seconds);
+        var date = new Date();
+        date.setSeconds(date.getSeconds() + plus_seconds);
 
-		var hours = ("0"+date.getHours()).slice(-2);
-		var minutes = ("0"+date.getMinutes()).slice(-2);
+        var hours = ("0"+date.getHours()).slice(-2);
+        var minutes = ("0"+date.getMinutes()).slice(-2);
 
-		return '(' + hours + ':' + minutes + ')';
-	}
+        return '(' + hours + ':' + minutes + ')';
+    }
 
 /*********************
 * func: コールバック
@@ -59,61 +59,61 @@ function after_seconds(n)
 * supple:
 *********************/
 function callback(json)
-	{
-		// jsonをlogに追加する
-	    for(var i = 0; i < json.length; i++)
-			{
-				row_number += 1;
+    {
+        // jsonをlogに追加する
+        for(var i = 0; i < json.length; i++)
+            {
+                row_number += 1;
 
                 // logに追加するデータ
-				var row            = row_number,
-				    question_title = json[i]['title'],
-					question_url   = json[i]['url'],
-				    time_stump       = after_seconds(i);
+                var row            = row_number,
+                    question_title = json[i]['title'],
+                    question_url   = json[i]['url'],
+                    time_stump       = after_seconds(i);
 
-				// HTMLに追加するエレメント
-				var div    = document.createElement('div'),
-				    href_1 = document.createElement('a'),
-				    span_1 = document.createElement('span');
-				    span_2 = document.createElement('span');
+                // HTMLに追加するエレメント
+                var div    = document.createElement('div'),
+                    href_1 = document.createElement('a'),
+                    span_1 = document.createElement('span');
+                    span_2 = document.createElement('span');
 
-				href_1.className = 'question_href';
-				span_1.className = 'row_number';
-				span_2.className = 'time_stump';
+                href_1.className = 'question_href';
+                span_1.className = 'row_number';
+                span_2.className = 'time_stump';
 
-				href_1.innerText = question_title;
-				href_1.href      = question_url;
-				span_1.innerText = row;
-				span_2.innerText = time_stump;
+                href_1.innerText = question_title;
+                href_1.href      = question_url;
+                span_1.innerText = row;
+                span_2.innerText = time_stump;
 
-				div.append(span_1);
-				div.append(href_1);
-				div.append(span_2);
+                div.append(span_1);
+                div.append(href_1);
+                div.append(span_2);
 
-				(function(elem,for_timeout)
-					{
-						if (for_timeout == 4) 
-							{
-						    	setTimeout(function(){insert_log(elem);
-													  console.log(new Date);
-													  var date = new Date;
-										          	  var current_seconds = ("0"+date.getSeconds()).slice(-2) + "000";
-													  console.log(current_seconds);
-												      var load_seconds = 66000 - current_seconds;
-													  console.log(load_seconds);
-												      setTimeout("load_json()", load_seconds);
-													 },
-										    10000*(for_timeout+1));
-							}
-						else
-							{
-						    	setTimeout(function(){insert_log(elem)},
-										    10000*(for_timeout+1));
-							}
-                	}
-			    )(div,i);
-			}
-	}
+                (function(elem,for_timeout)
+                    {
+                        if (for_timeout == 4) 
+                            {
+                                setTimeout(function(){insert_log(elem);
+                                                      console.log(new Date);
+                                                      var date = new Date;
+                                                      var current_seconds = ("0"+date.getSeconds()).slice(-2) + "000";
+                                                      console.log(current_seconds);
+                                                      var load_seconds = 66000 - current_seconds;
+                                                      console.log(load_seconds);
+                                                      setTimeout("load_json()", load_seconds);
+                                                     },
+                                            10000*(for_timeout+1));
+                            }
+                        else
+                            {
+                                setTimeout(function(){insert_log(elem)},
+                                            10000*(for_timeout+1));
+                            }
+                    }
+                )(div,i);
+            }
+    }
 
 //load_json();
 var date = new Date;
